41c1059a6f85a80d9e889f25968ebf0ac5898207
Marta Villalba <140154144+Martavo@users.noreply.github.com>
2023-12-27 20:16:36 +0100

    Correcciones OpticaScript.sql

--- Estructura de datos. Ej. 1/OpticaScript.sql
+++ Estructura de datos. Ej. 1/OpticaScript.sql
@@ -1,20 +1,21 @@
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+DROP DATABASE IF EXISTS Optica;
+CREATE DATABASE Optica CHARACTER SET utf8;
+USE Optica;
 
 -- -----------------------------------------------------
--- Table `mydb`.`proveedor`
+-- Table `proveedor`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`proveedor` (
+CREATE TABLE IF NOT EXISTS `proveedor` (
   `id` INT NOT NULL,
   `nombre` VARCHAR(45) NOT NULL,
-  `direccion` VARCHAR(150) NOT NULL,
-  `telefono` VARCHAR(45) NULL,
-  `fax` VARCHAR(45) NULL,
+  `direccion` TEXT(100) NOT NULL,
+  `telefono` INT NULL,
+  `fax` INT NULL,
   `NIF` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `telefono_UNIQUE` (`telefono` ASC) VISIBLE)
@@ -22,31 +23,34 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`gafas`
+-- Table `gafas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`gafas` (
+CREATE TABLE IF NOT EXISTS `gafas` (
   `id` INT NOT NULL,
   `marca` VARCHAR(45) NOT NULL,
-  `graduacion` INT NULL,
-  `montura` VARCHAR(45) NOT NULL,
   `color_montura` VARCHAR(45) NOT NULL,
-  `color_crital` VARCHAR(45) NOT NULL,
-  `precio` INT NOT NULL,
-  `id_proveedor` INT NOT NULL,
+  `color_cristal_dch` VARCHAR(45) NOT NULL,
+  `color_crital_izq` VARCHAR(45) NOT NULL,
+  `precio` DOUBLE NOT NULL,
+  `graduacion_izq` DOUBLE NULL,
+  `graducacion_dch` DOUBLE NULL,
+  `montura` ENUM('Flotante', 'Pasta', 'Metalicas') NULL,
+  `proveedor_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `id_proveedor_idx` (`id_proveedor` ASC) VISIBLE,
-  CONSTRAINT `id_proveedor`
-    FOREIGN KEY (`id_proveedor`)
-    REFERENCES `mydb`.`proveedor` (`id`)
+  INDEX `fk_gafas_proveedor1_idx` (`proveedor_id` ASC) VISIBLE,
+  CONSTRAINT `fk_gafas_proveedor1`
+    FOREIGN KEY (`proveedor_id`)
+    REFERENCES `proveedor` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
+
 -- -----------------------------------------------------
--- Table `mydb`.`cliente`
+-- Table `cliente`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`cliente` (
+CREATE TABLE IF NOT EXISTS `cliente` (
   `id` INT NOT NULL,
   `nombre` VARCHAR(155) NOT NULL,
   `direccion` VARCHAR(150) NOT NULL,
@@ -58,9 +62,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`empleado`
+-- Table `empleado`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`empleado` (
+CREATE TABLE IF NOT EXISTS `empleado` (
   `id` INT NOT NULL,
   `nombre` VARCHAR(45) NOT NULL,
   `apellido1` VARCHAR(45) NOT NULL,
@@ -70,78 +74,65 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`venta`
+-- Table `venta`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`venta` (
+CREATE TABLE IF NOT EXISTS `venta` (
   `id` INT NOT NULL,
   `id_empleado` INT NOT NULL,
   `id_clientes` INT NOT NULL,
+  `gafas_id` INT NOT NULL,
+  `tiempo_venta` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `id_empleado_idx` (`id_empleado` ASC) VISIBLE,
   INDEX `id_clientes_idx` (`id_clientes` ASC) VISIBLE,
+  INDEX `fk_venta_gafas1_idx` (`gafas_id` ASC) VISIBLE,
   CONSTRAINT `id_empleado`
     FOREIGN KEY (`id_empleado`)
-    REFERENCES `mydb`.`empleado` (`id`)
+    REFERENCES `empleado` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `id_clientes`
     FOREIGN KEY (`id_clientes`)
-    REFERENCES `mydb`.`cliente` (`id`)
+    REFERENCES `cliente` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_venta_gafas1`
+    FOREIGN KEY (`gafas_id`)
+    REFERENCES `gafas` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`recomendacion`
+-- Table `recomendacion`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`recomendacion` (
+CREATE TABLE IF NOT EXISTS `recomendacion` (
   `id` INT NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`cliente_has_recomendacion`
+-- Table `cliente_has_recomendacion`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`cliente_has_recomendacion` (
+CREATE TABLE IF NOT EXISTS `cliente_has_recomendacion` (
   `cliente_id` INT NOT NULL,
-  `recomendacion_id` INT NULL,
-  INDEX `recomendacion_id_idx` (`recomendacion_id` ASC) VISIBLE,
+  `recomendacion_id` INT NOT NULL,
   INDEX `cliente_id_idx` (`cliente_id` ASC) VISIBLE,
+  INDEX `fk_cliente_has_recomendacion_recomendacion1_idx` (`recomendacion_id` ASC) VISIBLE,
   CONSTRAINT `cliente_id`
     FOREIGN KEY (`cliente_id`)
-    REFERENCES `mydb`.`cliente` (`id`)
+    REFERENCES `cliente` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `recomendacion_id`
+  CONSTRAINT `fk_cliente_has_recomendacion_recomendacion1`
     FOREIGN KEY (`recomendacion_id`)
-    REFERENCES `mydb`.`recomendacion` (`id`)
+    REFERENCES `recomendacion` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
--- -----------------------------------------------------
--- Table `mydb`.`gafas_has_ventas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`gafas_has_ventas` (
-  `gafas_id` INT NOT NULL,
-  `venta_id` INT NOT NULL,
-  `tiempo_venta` DATETIME NOT NULL,
-  INDEX `gafas_id_idx` (`gafas_id` ASC) VISIBLE,
-  INDEX `venta_id_idx` (`venta_id` ASC) VISIBLE,
-  CONSTRAINT `gafas_id`
-    FOREIGN KEY (`gafas_id`)
-    REFERENCES `mydb`.`gafas` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `venta_id`
-    FOREIGN KEY (`venta_id`)
-    REFERENCES `mydb`.`venta` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
