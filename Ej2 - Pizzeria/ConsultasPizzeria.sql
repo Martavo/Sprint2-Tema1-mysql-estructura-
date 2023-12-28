91ec6e90b1ba18c59228ad80dd586093efc19e6f
Marta Villalba <140154144+Martavo@users.noreply.github.com>
2023-12-28 12:18:40 +0100

    Corrección ConsultasPizzeria.sql

--- Ej2 - Pizzeria/ConsultasPizzeria.sql
+++ Ej2 - Pizzeria/ConsultasPizzeria.sql
@@ -1,10 +1,9 @@
 -- CONSULTAS
 
 -- Llista quants productes de tipus “Begudes” s'han venut en una determinada localitat.
-SELECT COUNT(chp.Comanda_id) AS total_begudes_vendudes, cl.Ciudad FROM Comanda_has_Producto chp JOIN Producto p ON chp.Producto_id = p.id JOIN Comanda c ON chp.Comanda_id = c.id 
-JOIN Cliente cl ON c.Cliente_id = cl.id JOIN Bebida b ON p.Bebida_id = b.id WHERE b.id IS NOT NULL GROUP BY cl.Ciudad;
+SELECT COUNT(*) AS cantidad_productos_vendidos FROM Comanda_has_Producto AS cp JOIN Comanda AS c ON cp.Comanda_id = c.id JOIN Producto AS p ON cp.Producto_id = p.id JOIN Categoria_pizza AS cpizza ON p.Categoria_pizza_id = cpizza.id JOIN Tienda AS t ON c.tienda_id = t.id WHERE t.ciudad = 'Barcelona';
 
 -- Llista quantes comandes ha efectuat un determinat empleat/da.
 SELECT COUNT(Trabajador_has_Comanda.Comanda_id) AS total_comandes_efectuades, Trabajador.Nombre AS nombre_trabajador FROM Trabajador_has_Comanda JOIN Trabajador ON Trabajador_has_Comanda.Trabajador_id = Trabajador.id
-WHERE Trabajador_has_Comanda.Trabajador_id = 2;
+WHERE Trabajador_has_Comanda.Trabajador_id = 4;
 
